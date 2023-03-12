00fa8b2824e79a619208145af7d05b74dcc0e38b
Erik <erik.giesenloo@gmail.com>
2023-03-12 17:42:21 +0000

    Created quotes & changed asserts

---
+++ data/quotes.py
@@ -0,0 +1,22 @@
+# General quotes
+ELK_ZINNEN_DAH = "Elk zinnen dah!"
+USE_SLASH_COMMANDS = "We have migrated to slash commands. Please use `/{}` instead of `!{}`." # ARGS: commandName, commandName
+NOT_STAFF_ERROR = "You must be a staff member to use this command."
+NOT_POST_OWNER_ERROR = "You must be the owner of the post."
+NOT_POST_OWNER_OR_STAFF_ERROR = "You must be the owner of the post or a staff member"
+THREAD_ANSWERED = "Thread already marked as answered"
+TIMED_OUT = "Timed out!"
+
+# Channel manager quotes
+NOT_IN_FORUM_ERROR = "Not in forum"
+NOT_IN_KELDER_ERROR = "Je zit niet in #kelder"
+KELDER_LIMIET_ERROR = "De limiet moet tussen 3 en 8 liggen."
+KELDER_LIMIER_UPDATED = "De limiet is nu {}." # ARGS: limit
+# FAQ quotes
+UPDATING_FAQ = "Updating FAQs..."
+FAQ_UPDATED = "FAQ_s Updated"
+RUNNING_FAQ = "Running FAQ {}" # ARGS: label
+FAQ_ENDED = "FAQ ended."
+FAQ_DEREGISTERED = "FAQ '{}' verwijderd." # ARGS: faqName
+FAQ_REGISTERED = "FAQ '{}' is geregistreerd met label '{}' als begin." # ARGS: faqName, label
+NOT_FAQ_ERROR = "Er zit geen FAQ met de '{}' in de lijst." # ARGS: faqName
