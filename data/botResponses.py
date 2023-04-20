171ad23f8de128ad21534f45d3956a46b8b6a897
jassummisko <miskobozinoski@gmail.com>
2023-04-20 11:22:04 +0200

    Renamed quotes to botResponses (to reduce confusion with future quote cog)

--- data/botResponses.py
+++ data/botResponses.py
@@ -1,4 +1,4 @@
-# General quotes
+# General responses
 ELK_ZINNEN_DAH = "Elk zinnen dah!"
 USE_SLASH_COMMANDS = "We have migrated to slash commands. Please use `/{}` instead of `!{}`." # ARGS: commandName, commandName
 NOT_STAFF_ERROR = "You must be a staff member to use this command."
@@ -7,17 +7,17 @@ NOT_POST_OWNER_OR_STAFF_ERROR = "You must be the owner of the post or a staff me
 THREAD_ANSWERED = "Thread already marked as answered"
 TIMED_OUT = "Timed out!"
 
-# Role manager quotes
+# Role manager responses
 ROLE_GIVEN = "Role '{}' given" # ARGS: roleName
 ROLE_REMOVED = "Role '{}' removed" # ARGS: roleName
 
-# Channel manager quotes
+# Channel manager responses
 NOT_IN_FORUM_ERROR = "Not in forum"
 NOT_IN_KELDER_ERROR = "Je zit niet in #kelder"
 KELDER_LIMIET_ERROR = "De limiet moet tussen 3 en 8 liggen."
 KELDER_LIMIER_UPDATED = "De limiet is nu {}." # ARGS: limit
 
-# FAQ quotes
+# FAQ responses
 UPDATING_FAQ = "Updating FAQs..."
 FAQ_UPDATED = "FAQ_s Updated"
 RUNNING_FAQ = "Running FAQ {}" # ARGS: label
@@ -26,7 +26,7 @@ FAQ_DEREGISTERED = "FAQ '{}' verwijderd." # ARGS: faqName
 FAQ_REGISTERED = "FAQ '{}' is geregistreerd met label '{}' als begin." # ARGS: faqName, label
 NOT_FAQ_ERROR = "Er zit geen FAQ met de '{}' in de lijst." # ARGS: faqName
 
-# Scraper quotes
+# Scraper responses
 DEHET_NOWORD = "Ik heb geen woorden kunnen vinden voor '{}'"
 DEHET_NONOUN = "Ik heb geen substantieven kunnen vinden voor '{}'"
 DEHET_SINGLEWORD = "\"**{}** {}\""
