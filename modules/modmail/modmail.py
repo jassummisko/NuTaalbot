bbb16d9bfcf1b24f4493b5c8096ba90dd7f9cd7c
jassummisko <miskobozinoski@gmail.com>
2023-10-09 11:11:32 +0200

    Tried to fix tags

--- modules/modmail/modmail.py
+++ modules/modmail/modmail.py
@@ -127,35 +127,32 @@ async def sendMailWizard(bot: discord.Client, msg: discord.Message):
     )
     if author != "/": await thread_w_message.thread.send(botResponses.MAIL_RESPOND())
 
-    in_tag = mail_channel.get_tag(ModMailRoles.In)
-    assert(in_tag)
-    await thread_w_message.thread.add_tags(in_tag)
 
     def _getTag(enum: ModMailRoles) -> discord.ForumTag:
         tag = mail_channel.get_tag(enum)
         assert isinstance(tag, discord.ForumTag)
         return tag
 
-    type_tag: discord.ForumTag | None = None
-    staff_tag: discord.ForumTag | None = None
+    tags: list[discord.ForumTag] = []
+
+    tags.append(_getTag(ModMailRoles.In))
 
     match mailtype:
         case MailType.OTHER:
             pass
         case MailType.FEEDBACK:
-            type_tag = _getTag(ModMailRoles.Server)
-            staff_tag = _getTag(ModMailRoles.Mod)
+            tags.append(_getTag(ModMailRoles.Server))
+            tags.append(_getTag(ModMailRoles.Mod))
         case MailType.REPORT:
-            type_tag = _getTag(ModMailRoles.Report)
-            staff_tag = _getTag(ModMailRoles.Mod)
+            tags.append(_getTag(ModMailRoles.Report))
+            tags.append(_getTag(ModMailRoles.Mod))
         case MailType.NIVEAU:
-            type_tag = _getTag(ModMailRoles.Niveau)
+            tags.append(_getTag(ModMailRoles.Niveau))
         case MailType.SESSIE:
-            type_tag = _getTag(ModMailRoles.Server)
+            tags.append(_getTag(ModMailRoles.Server))
         case MailType.BOT:
-            type_tag = _getTag(ModMailRoles.Server)
+            tags.append(_getTag(ModMailRoles.Server))
 
-    if type_tag: await thread_w_message.thread.add_tags(type_tag)
-    if staff_tag: await thread_w_message.thread.add_tags(staff_tag)
+    await thread_w_message.thread.add_tags(*tags)
 
     await msg.channel.send(botResponses.MAIL_SENT("the staff team"))
